40a8938ae1ae6fff94cbba82c07a1e174dbb6376
Anu Bandi <anu@server.fake>
2014-01-25 20:16:27 -0500

    added email support

--- main.js
+++ main.js
@@ -8,12 +8,15 @@
 var numHeaderRows = 1;
 var numHeaderCols = 2;
 var numSubColumnPairs = 5;
-var docUrl = '';
 var toEmail = 'group@listserv.com';
+var emailSubject = 'Group Name';
 
 
 /*** Optional Params ***/
 var toastTimeout = 3;
+var headerBgColor = '#465C8E'; //darkBlue
+var borderColor = '#A2A3A4'; //lightBlue
+var altColor = '#EAEBED'; //veryLightBlue
 
 
 /*** Main Functions ***/
@@ -40,20 +43,8 @@ function onOpen() {
 
 // updates the colors of edited rows
 function onEdit(e) {
-  if (e.range.getGridId() == 0) {
-    for (var row = e.range.getRow(); row <= e.range.getLastRow(); row++) {
-      colorRow(row);
-    }
-  }
-}
-
-function sendEmail() {
-  SpreadsheetApp.getActiveSpreadsheet().toast(toEmail, 'Sending Email', toastTimeout);
-  firstSheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-
-  for (var row = 1 + numHeaderRows; row <= SpreadsheetApp.getActiveSpreadsheet().getSheets()[0].getMaxRows(); row++) {
-    date = firstSheet.getRange(row, 1).getValue()
-    rowRange = firstSheet.getRange(row, 1 + numHeaderCols, 1, numSubColumnPairs * 2)
+  for (var row = e.range.getRow(); row <= e.range.getLastRow(); row++) {
+    colorRow(row);
   }
 }
 
@@ -61,8 +52,6 @@ function sendEmail() {
 function updateColors() {
   var startRow = 1 + numHeaderRows;
   var endRow = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0].getMaxRows();
-  SpreadsheetApp.getActiveSpreadsheet().toast('updating rows ' + startRow + ' - ' + endRow, 'Started', toastTimeout);
-
   for (var row = startRow; row <= endRow; row++) {
     colorRow(row);
   }
@@ -74,11 +63,9 @@ function colorRow(row) {
     return;
   }
 
-  firstSheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-  date = firstSheet.getRange(row, 1).getValue()
-  rowRange = firstSheet.getRange(row, 1 + numHeaderCols, 1, numSubColumnPairs * 2)
-  var rowData = rowRange.getValues()[0];
-  var rowColors = [];
+  var firstSheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+  var date = firstSheet.getRange(row, 1).getValue();
+  var rowRange = firstSheet.getRange(row, 1 + numHeaderCols, 1, numSubColumnPairs * 2);
 
   if (!(date instanceof Date)) {
     rowRange.setBackground('white');
@@ -89,6 +76,9 @@ function colorRow(row) {
     rowRange.setBackground('grey');
     return;
   }
+
+  var rowData = rowRange.getValues()[0];
+  var rowColors = [];
 
   for (var col = 0; col < rowData.length; col += 2) {
     var orig = rowData[col];
@@ -106,8 +96,79 @@ function colorRow(row) {
   rowRange.setBackgrounds([rowColors]);
 }
 
+// sends an email digest with sub requests
+function sendEmail() {
+  var firstSheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+  var names = [],
+    dates = [],
+    reasons = [];
+
+  for (var row = 1 + numHeaderRows; row <= firstSheet.getMaxRows(); row++) {
+    var date = firstSheet.getRange(row, 1).getValue();
+
+    if ((date instanceof Date) && dateTodayDiff(date) >= 0) {
+      var rowRange = firstSheet.getRange(row, 1 + numHeaderCols, 1, numSubColumnPairs * 2);
+      var rowData = rowRange.getValues()[0];
+      var rowNotes = rowRange.getNotes()[0];
+      for (var col = 0; col < rowData.length; col += 2) {
+        var orig = rowData[col];
+        var sub = rowData[col + 1];
+        if (orig != '' && sub == '') {
+          names.push(orig);
+          dates.push(date);
+          var r = rowNotes[col];
+          if (r == '') {
+            r = '{reason not specified. please add reason as a NOTE in gdoc}';
+          }
+          reasons.push(r);
+        }
+      }
+    }
+  }
+
+
+  if (names.length > 0) {
+    var htmlMsg = createHTMLTable(names, dates, reasons);
+    htmlMsg += '<br><br><br><br> *This message was automatically generated at ' + formatToday('h:mm a (M/d/yy)') + ' by ' + appName + ' [' + appUrl + '].';
+    var subject = emailSubject + ' ' + formatToday('M/d') + ' Digest [' + names.length + ' needed]';
+    MailApp.sendEmail(toEmail, subject, "Your email client doesn't support HTML :(", {
+      htmlBody: htmlMsg
+    });
+    SpreadsheetApp.getActiveSpreadsheet().toast(names.length + ' people need subs', 'Emailed ' + toEmail, toastTimeout);
+  } else {
+    SpreadsheetApp.getActiveSpreadsheet().toast('No one needs a sub', 'Email not sent', toastTimeout);
+  }
+}
+
+// returns a formatted html table
+function createHTMLTable(nameList, dateList, reasonList) {
+  var HTMLtable = wrapHTML('Sub Requests', 'h2', 'style="color:' + headerBgColor + '"');
+  HTMLtable += '<table  cellpadding="5" style="border:1px solid ' + headerBgColor + ';border-collapse:collapse;width:100%">';
+  var hDate = wrapHTML('Date', 'th', 'width="15%" align="center"');
+  var hName = wrapHTML('Name', 'th', 'width="15%" align="center"');
+  var hMessage = wrapHTML('Message', 'th', 'width="70%" align="center"');
+  HTMLtable += wrapHTML(hDate + hName + hMessage, 'tr', 'style="background-color:' + headerBgColor + ';color:white"');
+
+  for (var i = 0; i < nameList.length; i++) {
+    var bgColor = '';
+    if (i % 2 != 0) bgColor = 'bgcolor="' + altColor + '" ';
+
+    var cellStyle = 'style="border:1px solid ' + borderColor + '"';
+    hDate = wrapHTML(formatDate(dateList[i], 'M/d (EEE)'), 'td', cellStyle);
+    hName = wrapHTML(nameList[i], 'td', cellStyle);
+    hMessage = wrapHTML(reasonList[i], 'td', cellStyle);
+    HTMLtable += wrapHTML(hDate + hName + hMessage, 'tr', bgColor + cellStyle);
+  }
+
+  HTMLtable += "</table>";
+  HTMLtable += '<br>Sign up: ';
+  HTMLtable += wrapHTML('LINK', 'a', 'href="' + SpreadsheetApp.getActive().getUrl() + '"');
+  return HTMLtable;
+}
+
+// displays app info
 function about() {
-  SpreadsheetApp.getActiveSpreadsheet().toast('http://ultimateanu.github.io/gdoc-sub-system', 'gDoc Sub System');
+  SpreadsheetApp.getActiveSpreadsheet().toast(appUrl, appName);
 }
 
 
@@ -119,3 +180,19 @@ function dateTodayDiff(d) {
   var todayUtc = Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
   return Math.floor((dateUtc - todayUtc) / (1000 * 60 * 60 * 24));
 }
+
+function wrapHTML(inside, html, options) {
+  return '<' + html + ' ' + options + '>' + inside + '</' + html + '>';
+}
+
+function formatDate(d, f) {
+  return Utilities.formatDate(d, Session.getTimeZone(), f);
+}
+
+function formatToday(f) {
+  return formatDate(new Date(), f);
+}
+
+/*** App Params ***/
+var appName = 'gDoc Sub System';
+var appUrl = 'http://ultimateanu.github.io/gdoc-sub-system';
